43d9f60b06764480e8e5d3e14d6fa14dc3bc72c8
Karthic Rao <karthic@minio.io>
2018-09-14 12:51:31 +0530

    Adding test

---
+++ tests/app_tests.py
@@ -0,0 +1,19 @@
+import unittest
+import uuid
+
+import app
+
+# Helper
+
+def bytes_to_str(b):
+    return ''.join(str(x) for x in (b))
+    
+class AppTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.client = app.app.test_client()
+
+
+    def test_brigade_ci_meetup(self):
+        resp = self.client.get('/')
+        assert uuid.UUID(bytes_to_str(resp.data))

76c9aea6971000b9c3d35d40d8c287add0a57509
SandeepVaman <sandeepvbujire@gmail.com>
2018-09-19 16:49:24 +0530

    Added server details

--- app/__init__.py
+++ app/__init__.py
@@ -7,3 +7,6 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return Response(str(uuid.uuid4()), status=200, mimetype='text/plain')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)

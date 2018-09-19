d0e69b206cdbef5558c9ca4ec48e063ee10455a6
SandeepVaman <sandeepvbujire@gmail.com>
2018-09-19 20:13:47 +0530

    Modfied brigade js

--- brigade.js
+++ brigade.js
@@ -1,4 +1,5 @@
-const { events, Job } = require("brigadier");
+const { events, Job, Group } = require("brigadier");
+const checkRunImage = "technosophos/brigade-github-check-run:latest"
 
 events.on("check_suite:requested", function(e, project){
   slackNotify("danger", "CHECKS_API", "CHECKS_API", e).run();
@@ -9,6 +10,53 @@ events.on("check_suite:rerequested", function(e, project){
 events.on("check_run:rerequested", function(e, project){
   slackNotify("danger", "CHECKS_API", "CHECKS_API", e).run();
 });
+
+events.on("pull_request", checkRequested);
+
+function checkRequested(e, p) {
+  console.log("check requested")
+  // Common configuration
+  const env = {
+    CHECK_PAYLOAD: e.payload,
+    CHECK_NAME: "MyService",
+    CHECK_TITLE: "Echo Test",
+  }
+
+  // This will represent our build job. For us, it's just an empty thinger.
+  const build = new Job("build", "alpine:3.7", ["sleep 60", "echo hello"])
+
+  // For convenience, we'll create three jobs: one for each GitHub Check
+  // stage.
+  const start = new Job("start-run", checkRunImage)
+  start.imageForcePull = true
+  start.env = env
+  start.env.CHECK_SUMMARY = "Beginning test run"
+
+  const end = new Job("end-run", checkRunImage)
+  end.imageForcePull = true
+  end.env = env
+
+  // Now we run the jobs in order:
+  // - Notify GitHub of start
+  // - Run the test
+  // - Notify GitHub of completion
+  //
+  // On error, we catch the error and notify GitHub of a failure.
+  start.run().then(() => {
+    return build.run()
+  }).then( (result) => {
+    end.env.CHECK_CONCLUSION = "success"
+    end.env.CHECK_SUMMARY = "Build completed"
+    end.env.CHECK_TEXT = result.toString()
+    return end.run()
+  }).catch( (err) => {
+    // In this case, we mark the ending failed.
+    end.env.CHECK_CONCLUSION = "failed"
+    end.env.CHECK_SUMMARY = "Build failed"
+    end.env.CHECK_TEXT = `Error: ${ err }`
+    return end.run()
+  })
+}
 
 // events.on("pull_request", function(e, project) {
 //   console.log("received push for commit " + e.revision.commit);
